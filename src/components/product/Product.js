26b6eceeab00b8a88cbb31896e75bd4b5655c037
kanakweb <kanakwebdev@gmail.com>
2022-03-22 23:03:17 +0600

    ema-jon build

---
+++ src/components/product/Product.js
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    
+    const{name,img,seller,ratings,price}=props.product
+  
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+            <p>Price : $ {price}</p>
+            <p><small>Seller : {seller}</small></p>
+            <p><small>Ratings : {ratings}</small></p>
+            </div>
+            <button onClick={()=>props.handleClick(props.product)} className='btn-cart'><p>Add to Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/shop/Shop.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../product/Product';
+import './shop.css'
+
+const Shop = () => {
+    const [products,setProducts]=useState([])
+
+    const[cart,setCart]=useState([])
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts (data))
+    },[])
+
+    const handleClick=(product)=>{
+        const newCart=[...cart , product]
+        setCart(newCart)
+    }
+    return (
+        <div className='shop-container'>
+         <div className="product-container">
+       {
+           products.map(product=>
+               <Product 
+               key={product.id}
+               product={product}
+               handleClick={handleClick}
+               ></Product>
+           )
+       }
+         </div>
+         <div className="cart-container">
+            <h4>Product Summary</h4>
+            <p>Select Item:{cart.length}</p>
+        </div> 
+        </div>
+    );
+};
+
+export default Shop;
