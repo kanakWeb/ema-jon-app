11db1cbaf313c85ba99b50d01647abe810da0b34
kanakweb <kanakwebdev@gmail.com>
2022-03-23 15:05:55 +0600

    Cart added local storage etc

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../product/Product';
 import './shop.css'
@@ -14,9 +15,19 @@ const Shop = () => {
         .then(data=>setProducts (data))
     },[])
 
+    useEffect(()=>{
+        const storedCart=getStoredCart()
+        // console.log(storedCart)
+        for(const id in storedCart){
+            console.log(id);
+        }
+    },[])
+
     const handleClick=(product)=>{
+        const{id}=product
         const newCart=[...cart , product]
         setCart(newCart)
+        addToDb(id)
     }
     return (
         <div className='shop-container'>
