ce408bdd1601aaaa7432076b67f4e64cc6af07de
kanakweb <kanakwebdev@gmail.com>
2022-03-23 13:41:29 +0600

    Cart added tota,tax etc

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = (props) => {
+    const{Cart}=props;
+
+    let total=0;
+    let shipping=0;
+
+Cart.forEach(cart => {
+    total=total+cart.price;
+    shipping=shipping+cart.shipping
+});
+
+const tax=parseFloat((total*0.1).toFixed(2));
+const grandTotal=total+shipping+tax;
     return (
-        <div >
+        <div className='cart-container'>
              <h4>Order Summary</h4>
-        <p>Select Item:{props.Cart.length}</p>
+        <p>Select Item:{Cart.length}</p>
+        <p>Total Price:$ {total} </p>
+        <p>Total Shipping: {shipping}</p>
+        <p>Tax: {tax}</p>
+        <h5>Grand Total:$ {grandTotal.toFixed(2)} </h5>
         </div>
     );
 };
