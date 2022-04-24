120cc51fa60119aefb4037d7345f0106b6c9740d
kanakweb <kanakwebdev@gmail.com>
2022-04-24 18:46:01 +0600

    client side

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,39 +1,46 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProduct from "../../hooks/useProduct";
-import { removeFromDb } from "../../utilities/fakedb";
+import {removeFromDb} from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import ReviewItem from "../Reviewitem/ReviewItem";
 
 const Orders = () => {
-  const [products] = useProduct();
- 
-  const [cart, setCart] = useCart(products);
-  const navigate = useNavigate();
-  const handleRemove = product => {
-      const rest=cart.filter(pd=>pd.id!==product.id)
-      setCart(rest)
-      removeFromDb(product.id)
-  };
-  return (
-    <div className="shop-container">
-      <div className="review-items-container">
-        {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}
-          handleRemove={handleRemove}
-          ></ReviewItem>
-        ))}
-      </div>
-      <div className="cart-container">
-        <Cart Cart={cart}>
-           
-            <button className='btn btn-warning mt-4' onClick={()=>navigate('/shipment')}>Proceed Shipping </button>
-         
-        </Cart>
-      </div>
-    </div>
-  );
+    const [products] = useProduct();
+
+    const [cart, setCart] = useCart();
+    const navigate = useNavigate();
+    const handleRemove = product => {
+        const rest = cart.filter(pd => pd._id !== product._id)
+        setCart(rest)
+        removeFromDb(product._id)
+    };
+    return (
+        <div className="shop-container">
+            <div className="review-items-container">
+                {
+                cart.map((product) => (
+                    <ReviewItem key={
+                            product._id
+                        }
+                        product={product}
+                        handleRemove={handleRemove}></ReviewItem>
+                ))
+            } </div>
+            <div className="cart-container">
+                <Cart Cart={cart}>
+
+                    <button className='btn btn-warning mt-4'
+                        onClick={
+                            () => navigate('/shipment')
+                    }>Proceed Shipping
+                    </button>
+
+                </Cart>
+            </div>
+        </div>
+    );
 };
 
 export default Orders;
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,68 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import useProduct from '../../hooks/useProduct';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import useCart from '../../hooks/useCart';
+import {addToDb, getStoredCart} from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../product/Product';
 import './shop.css'
 
 const Shop = () => {
-    const [products]=useProduct([])
+   
 
-    const[cart,setCart]=useState([])
+    const [cart, setCart] = useCart()
+    const[pageCount,SetPageCount]=useState(0)
+    const[page,setPage]=useState(0)
+    const[size,setSize]=useState(10)
+
+
+    const[products,setProducts]=useState([])
 
     useEffect(()=>{
-        const storedCart=getStoredCart()
-        // console.log(storedCart)
-        const saveCart=[];
-        for(const id in storedCart){
-            const addedProduct=products.find(product=>product.id===id)
-            if(addedProduct){
-                const quantity=storedCart[id];
-                addedProduct.quantity=quantity;
-                saveCart.push(addedProduct)
-            }
-        }
-        setCart(saveCart)
-    },[products])
+        fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
 
-    const handleClick=(selecteProduct)=>{
-    //    const{id}=product
-        let newCart=[];
-        const exists=cart.find(product=>product.id===selecteProduct.id)
-        if(!exists){
-            selecteProduct.quantity=1;
-           newCart=[...cart , selecteProduct]
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[page,size])
+
+
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/productCount')
+        .then(res=>res.json())
+        .then(data=>{
+            const count=data.count;
+         const page=Math.ceil(count/10)
+         SetPageCount(page)
+        })
+    },[])
+
+
+  
+
+    const handleClick = (selectProduct) => { //    const{id}=product
+        let newCart = [];
+        const exists = cart.find(product => product._id === selectProduct._id)
+        if (! exists) {
+            selectProduct.quantity = 1;
+            newCart = [
+                ...cart,
+                selectProduct
+            ]
+        } else {
+            const rest = cart.filter(product => product._id !== selectProduct._id)
+            exists.quantity = exists.quantity + 1;
+            newCart = [
+                ... rest,
+                exists
+            ]
         }
-        else{
-            const rest=cart.filter(product=>product.id!==selecteProduct.id)
-            exists.quantity=exists.quantity+1;
-            newCart=[...rest,exists]
-        }
-        
+
         setCart(newCart)
-        addToDb(selecteProduct.id)
+        addToDb(selectProduct._id)
     }
     return (
         <div className='shop-container'>
-         <div className="product-container">
-       {
-           products.map(product=>
-               <Product 
-               key={product.id}
-               product={product}
-               handleClick={handleClick}
-               ></Product>
-           )
-       }
-         </div>
-         <div className="cart-container">
-           <Cart Cart={cart}>
-           <Link to='/orders'>
-               <button className='btn btn-warning mt-4'>Review Order </button>
-           </Link>
-           </Cart>
-        </div> 
+            <div className="product-container">
+                {
+                products.map(product => <Product key={
+                        product._id
+                    }
+                    product={product}
+                    handleClick={handleClick}></Product>)
+            }
+            <div>
+               
+            <div className='pagination my-5 mx-auto'>
+            {
+                    [...Array(pageCount).keys()].map(number=><button onClick={()=>setPage(number)} className={page===number?'selected':''}>{number+1}</button>)
+                }
+                
+                <select onChange={e=>setSize(e.target.value)} name="" id="">
+                    <option value="5">5</option>   
+                    <option value="10" selected>10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
+            </div>
+            </div>
+            
+             </div>
+             
+            <div className="cart-container">
+                <Cart Cart={cart}>
+                    <Link to='/orders'>
+                        <button className='btn btn-warning mt-4'>Review Order
+                        </button>
+                    </Link>
+                </Cart>
+            </div>
+            
         </div>
     );
 };
